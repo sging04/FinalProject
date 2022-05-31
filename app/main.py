a1cd671e6487f7551ce74d50ae90910c124b9bd1
sging04 <sging2356@gmail.com>
2022-05-31 11:07:06 -0400

    additions

--- app/main.py
+++ app/main.py
@@ -1,48 +1,49 @@
 from flask import Flask, request, redirect, session, render_template, url_for
-from database import Database
 import json
 from random import *
 import os
 import sqlite3
 
-from database import UsernamePasswordTable, BlogTable #using database classes
+from database import UsernamePasswordTable #using database classes
 
-myapp= flask(__name__)
+app= Flask(__name__)
+app.secret_key = os.urandom(32)
 
 
 db_file = "data.db"
 
 
 userpass = UsernamePasswordTable(db_file, "userpass")
-blog = BlogTable(db_file, "blog")
-@myapp.route("/")
-def homepage():
-    return render_template("index.html")
+@app.route("/",  methods=["GET", "POST"])
+def disp_loginpage():
+    if (session.get("username") is not None):
+        # if there's an existing session, shows welcome page
+        if request.method == "GET":
+            # if there's an existing session, shows welcome page
+            return render_template( 'response.html', username=session.get("username") )
+        else:
+            topic = request.form['topic']
+            data = blog.seeContent()
+            return render_template( 'response.html', username=session.get("username"), topic=topic, data=data )
+    if ("username" != None):
+        return render_template( 'login.html' )
 
-@myapp.route("/", methods = ["POST"])
-def checklogin():
-    username = request.form["username"]
-    password = request.form["password"]
 
-       if (username=="" or password==""):
-        return render_template('login.html', syntaxerror="Cannot submit blank username or password")
-    elif not userpass.userExists(username):
-        return render_template('login.html', syntaxerror="Username does not exist")
-    elif not userpass.passMatch(username, password):
-        return render_template('login.html', syntaxerror = "Incorrect password")
-    else:
-        session["username"] = username
-        return redirect('/sloggedin')
 
-@myapp.route("/loggedin")
+@app.route("/loggedin")
 def loggedin():
-    return redirect("/index.html")
+    return redirect("/")
 
-@myapp.route("/signup")
+@app.route("/signup", methods=["POST","GET"])
 def signup():
-    username= request.args['username']
-    password= request.args['password']
-    passauth= request.args['passauth']
+    if session['username'] is not none:
+        redirect("loggedin")
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        username= request.args['username']
+        password= request.args['password']
+        passauth= request.args['passauth']
         userpass.insert(username, password) # committing actions to database must be done every time you commit a command
         session["username"]=username
         return redirect("/loggedin")
@@ -54,4 +55,5 @@ def signup():
 
 
 if __name__ == "__main__":
-    myapp.run()
+    app.run(debug=True)
+

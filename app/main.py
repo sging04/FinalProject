08c181a2a439df4883750e8865cd390754096a80
sging04 <sging2356@gmail.com>
2022-06-05 13:40:08 -0400

    templating

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, redirect, session, render_template, url_for
 import json
-from random import *
 import os
 import sqlite3
 
@@ -9,35 +8,29 @@ from database import UsernamePasswordTable #using database classes
 app= Flask(__name__)
 app.secret_key = os.urandom(32)
 
-
 db_file = "data.db"
 
 
 userpass = UsernamePasswordTable(db_file, "userpass")
-@app.route("/",  methods=["GET", "POST"])
+
+
+@app.route("/",  methods=["GET"])
 def disp_loginpage():
-    if (session.get("username") is not None):
+    if (not session.get("username") is None):
+        # if 
         # if there's an existing session, shows welcome page
         if request.method == "GET":
             # if there's an existing session, shows welcome page
-            return render_template( 'response.html', username=session.get("username") )
-        else:
-            topic = request.form['topic']
-            data = blog.seeContent()
-            return render_template( 'response.html', username=session.get("username"), topic=topic, data=data )
+            return redirect("/home")
+
     if ("username" != None):
         return render_template( 'login.html' )
 
 
-
-@app.route("/loggedin")
-def loggedin():
-    return redirect("/")
-
 @app.route("/signup", methods=["POST","GET"])
 def signup():
     if session['username'] is not none:
-        redirect("loggedin")
+        redirect("/")
     if request.method == "GET":
         return render_template("signup.html")
     elif request.method == "POST":
@@ -46,14 +39,13 @@ def signup():
         passauth= request.args['passauth']
         userpass.insert(username, password) # committing actions to database must be done every time you commit a command
         session["username"]=username
-        return redirect("/loggedin")
-    
+        return redirect("/home", username=session.get("username"))
 
-
-
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/home", methods=["GET"])
+def home():
+    return render_template(
+        "home.html",
+        username=session.get("username"),
+        decks = [1,2,3,3]
+        )
 
